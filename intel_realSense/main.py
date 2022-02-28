0beaa3e03962c6cdfa5f7065c8499599a2924fdb
tony19990828 <58881058+tony19990828@users.noreply.github.com>
2022-02-28 16:26:11 +0800

    Add files via upload

---
+++ intel_realSense/main.py
@@ -0,0 +1,23 @@
+from create_camera import *
+rs = IntelRealSense()
+try:
+    while True:
+        ret, color_image, depth_image = rs.get_frame_stream()
+        if not ret:
+            continue
+        depth_image_3d = np.dstack(
+            (depth_image, depth_image, depth_image))  # depth image is 1 channel, color is 3 channels
+        rs.save_pic(color_image, depth_image_3d)
+
+        depth_colormap = cv2.applyColorMap(cv2.convertScaleAbs(depth_image_3d, alpha=0.03), cv2.COLORMAP_JET)
+        images = np.hstack((depth_colormap, color_image))
+        key = cv2.waitKey(1)
+        cv2.namedWindow('Align Example', cv2.WINDOW_NORMAL)
+        cv2.imshow('Align Example', images)
+
+        # Press esc or 'q' to close the image window
+        if key & 0xFF == ord('q') or key == 27:
+            cv2.destroyAllWindows()
+            break
+finally:
+    rs.pipeline.stop()
